1f75cd5cdb5d96dda2b150040cfa61f0834b1f67
Narek2481 <narekengibaryan.1@mail.ru>
2023-03-07 17:07:46 +0400

    client-server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
-import express  from "express";
+import express from "express";
+import path from "path"
+import bp from "body-parser";
+import cors from "cors";
+import true_email from "./true_email.js"
 
 const app = express();
 
-app.listen(4000,() => console.log("ok port 4000")); 
-app.get("/",(req,res) => {
-    res.send({name:"Narek"});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.listen(4000);
+app.get("/" , (req, res) => {
+    res.send('ok');
+});
+app.post("/registr_submit", (req, res) => {
+    true_email(req.body.email)
+        .then(result => {
+            console.log(result); // true
+        })
+        .catch(error => {
+            console.log(error); // false
+        });
+    console.log(req.body);
+    res.send("poxos");
+});
+app.post("/login_submit", (req, res) => {
+    console.log(req.body);
+    res.send("all good");
 });
