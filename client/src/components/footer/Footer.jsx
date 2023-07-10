52bd3e43f0e1d19b5c6f30f84ccc8fad5f7c0182
Narek2481 <narekengibaryan.1@mail.ru>
2023-07-10 15:45:56 +0400

    ...

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { memo } from "react";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../redux/user/userSlice";
@@ -17,16 +17,16 @@ export default memo(function Footer() {
     return (
         <footer className="text-center">
             <ul className="row">
-                <li className={loginState.register_or_login ? "col-6" :"col-4" }>
+                <li className={loginState.register_or_login ? "col-6" : "col-4"}>
                     <Link to='/home'>Home</Link>
                 </li>
-                <li className={loginState.register_or_login ? "col-6" :"col-4" }>
+                <li className={loginState.register_or_login ? "col-6" : "col-4"}>
                     <Link to="/About_us">About us</Link>
                 </li>
-                {loginState.register_or_login ? "":<li className="col-4" >
+                {loginState.register_or_login ? "" : <li className="col-4" >
                     <Link to="/Sign_in">Sign in</Link>
                 </li>}
-                
+
                 <Social props={{ text: "instagram", src: instagram }}></Social>
                 <Social props={{ text: "twitter", src: twitter }}></Social>
                 <Social props={{ text: "gmail", src: gmail }}></Social>
